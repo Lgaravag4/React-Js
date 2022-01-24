90fe24e7b7efa30934cb0147a2d0007dd32c935f
Lgaravag4 <leandrogaravaglia@gmail.com>
2022-01-24 16:53:26 -0300

    Entregable clase 3

--- broches-garavaglia/src/App.js
+++ broches-garavaglia/src/App.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 
--- broches-garavaglia/src/components/NavBar.js
+++ broches-garavaglia/src/components/NavBar.js
@@ -2,21 +2,22 @@ import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+
+        <Navbar bg="dark" expand="xl">
             <Container>
-                <Navbar.Brand href="#home">Que-Broches</Navbar.Brand>
+                <Navbar.Brand href="#home" style={{color: "blue"}}>Que-Broches</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <Nav.Link href="#home" style={{color: "blue"}}>Productos</Nav.Link>
+                        <Nav.Link href="#link" style={{color: "blue"}}>Contacto</Nav.Link>
+                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown" style={{color: "blue"}}>
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
